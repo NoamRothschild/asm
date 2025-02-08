e390936bebd0400e5c3f1e627071018b02cea472
Noam Rothschild <noamr.lego@gmail.com>
2025-02-09 00:52:44 +0200

    lowercased printHex function

--- common/debug.asm
+++ common/debug.asm
@@ -36,7 +36,7 @@ printBin:
     pop ebp
     ret 8
 
-hexMap: db '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F',0
+hexMap: db '0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',0
 printHex:
     push ebp
     mov ebp, esp
